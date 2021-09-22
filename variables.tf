e56ea224b76e22c531ce055267ad58bbe5b015a6
ritiwa_acxiom <rituraj.Tiwari@acxiom.com>
2021-09-22 18:49:37 +0530

    updated

--- variables.tf
+++ variables.tf
@@ -97,3 +97,54 @@ variable "mtu" {
   description = "The network MTU. Must be a value between 1460 and 1500 inclusive. If set to 0 (meaning MTU is unset), the network will default to 1460 automatically."
   default     = 0
 }
+
+
+/*############For Peering################*/
+
+variable "prefix" {
+  description = "Name prefix for the network peerings"
+  type        = string
+  default     = "network-peering"
+}
+
+variable "local_network" {
+  description = "Resource link of the network to add a peering to."
+  type        = string
+  default = "https://www.googleapis.com/compute/v1/projects/my-test-project-324014/global/networks/hub-vpc"
+}
+
+variable "peer_network" {
+  description = "Resource link of the peer network."
+  type        = string
+  default = "https://www.googleapis.com/compute/v1/projects/my-test-project-324014/global/networks/udl-nw"
+}
+
+variable "export_peer_custom_routes" {
+  description = "Export custom routes to local network from peer network."
+  type        = bool
+  default     = false
+}
+
+variable "export_local_custom_routes" {
+  description = "Export custom routes to peer network from local network."
+  type        = bool
+  default     = false
+}
+
+variable "export_peer_subnet_routes_with_public_ip" {
+  description = "Export custom routes to local network from peer network (defaults to false; causes the Local Peering Connection to align with the [provider default](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network_peering#import_subnet_routes_with_public_ip), and the Remote Peering Connection to be opposite the provider default)."
+  type        = bool
+  default     = false
+}
+
+variable "export_local_subnet_routes_with_public_ip" {
+  description = "Export custom routes to peer network from local network (defaults to true; causes the Local Peering Connection to align with the [provider default](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network_peering#export_subnet_routes_with_public_ip), and the Remote Peering Connection to be opposite the provider default)."
+  type        = bool
+  default     = true
+}
+
+variable "module_depends_on" {
+  description = "List of modules or resources this module depends on."
+  type        = list(any)
+  default     = []
+}
